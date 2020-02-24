146dcc1bbf65d35f82f9e62d2033062cb70a7e77
Akilah Briscoe <59670842+akilahbrisc@users.noreply.github.com>
2020-02-24 17:50:12 -0500

    authorspage

--- src/Authors/Authors.js
+++ src/Authors/Authors.js
@@ -1,13 +1,34 @@
-import React from "react";
+import React, {Component} from "react";
 import "./Authors.css";
+import { AuthorsDisplay } from "./AuthorsDisplay.js";
 
-export default function Authors() {
-    return (
-        <div className="Authors"> 
-            <div className="lander">
-                <h1>Authors</h1>
-                <p>Here are Authors</p>
+export default class Authors extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            authors: [
+                {
+                id: 1,
+                author: "John Doe",
+                },
+                {
+                id: 2,
+                author: "Jung Kook",
+                },
+                {
+                id: 3,
+                author: "Levi Ackerman",
+                }
+            ]
+        };
+    }
+    render() {
+        return (
+            <div className="Authors">
+                <div className="lander">
+                    <AuthorsDisplay authors={this.state.authors} />
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
--- src/Books/Books.js
+++ src/Books/Books.js
@@ -1,13 +1,72 @@
-import React from "react";
+import React, {Component} from "react";
 import "./Books.css";
+import { BooksDisplay } from "./BooksDisplay.js";
 
-export default function Books() {
-    return (
-        <div className="Books"> 
-            <div className="lander">
-                <h1>Books</h1>
-                <p>Here are Books</p>
+export default class Books extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            books: [
+                {
+                    id:1,
+                    title: "Domain Driven Design",
+                    author: "Eric Evans",
+                    isbn: "978-0321125217"
+                },
+                {
+                    id:2,
+                    title: "Accelerate",
+                    author: "Nicole Forsgren",
+                    isbn: "978-1942788331"
+                },
+                {
+                    id:3,
+                    title: "Tiger's Curse",
+                    author: "Colleen Houck",
+                    isbn: "978-9478612776"
+                },
+                {
+                    id:4,
+                    title: "Pink Panther",
+                    author: "Jerry Beck",
+                    isbn: "978-4735861248"
+                },
+                {
+                    id:5,
+                    title: "Parry Hotter",
+                    author: "J.K. Rowling",
+                    isbn: "978-7698314297"
+                },
+                {
+                    id:6,
+                    title: "Parasite",
+                    author: "Bong Joon Ho",
+                    isbn: "978-3186975185"
+                },
+                {
+                    id:7,
+                    title: "Jane Eyre",
+                    author: "Charlotte Bronte",
+                    isbn: "978-6871654812"
+                },
+                {
+                    id:8,
+                    title: "Chocolate",
+                    author: "Kinder Bueno",
+                    isbn: "978-2291537654"
+                }
+            ]
+            
+        };
+    }
+    render() {
+        return (
+            <div className="Books">
+                <div className="lander">
+                    <BooksDisplay books={this.state.books} />
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
+
